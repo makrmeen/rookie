52d1bfee33a1cc555a9ec6cf88580234471cc866
makrmeen <makrmeen@gmail.com>
2021-03-19 16:17:39 +0100

    formulas javascript

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-//function name(pa, ram, meters) {code to be executed, by the function ;}
+/* FORMULAS
+ const name = document.querySelector(".name");
+
+
+ function loop(x) {
+  if (x >= 10) // "x >= 10" es la condición de salida (equivalente a "!(x < 10)")
+    return;
+  // hacer cosas
+  loop(x + 1); // la llamada recursiva
+}
+loop(0);
+
+const name = function(number) { return number * number }
+var x = square(4)
+function myConcat(separator) {
+    var result = ''; // inicia list
+    var i;
+    // itera a través de arguments
+    for (i = 1; i < arguments.length; i++) {
+       result += arguments[i] + separator;
+    }
+    return result;
+ }
+
+*/
