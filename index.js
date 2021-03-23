046e55871aaaa883dbecaf55a6ba9406f2e44029
makrmeen <makrmeen@gmail.com>
2021-03-23 10:47:48 +0100

    added the work from replit

--- index.js
+++ index.js
@@ -11,6 +11,71 @@ closeMenu.addEventListener('click', (close) => {
 });
 
 
+const sportContent = [
+    {
+      name: "Tennis",
+      picture: "https://i.ibb.co/kKq7Wqs/Schermata-2021-03-23-alle-10-24-05.png",
+      intro: "Tennis is a cool sport that I can practice individuallu or in gruops."
+    },
+    {
+      name: "Basketball",
+      picture: "https://placekitten.com/200/287",
+      intro: "Basketball is a cool sport that I can practice individuallu or in gruops."
+    },
+    {
+      name: "Surf",
+      picture: "https://i.ibb.co/0V27ZcR/pexels-elle-hughes-1549180.jpg",
+      intro: "Surf is a cool sport that I can practice individuallu or in gruops."
+    },
+    {
+      name: "Climbing",
+      picture: "https://placekitten.com/200/287",
+      intro: "Climbing is a cool sport that I can practice individuallu or in gruops."
+    },
+    {
+      name: "Soccer",
+      picture: "https://placekitten.com/200/287",
+      intro: "Soccer is a cool sport that I can practice individuallu or in gruops."
+    }
+  ];
+  
+  
+  const createSport = (event) => {
+  
+  //change page
+    window.location.href = "sport.html";
+  
+  //add event click target id
+     const x = event.target;
+     const clickedId = x.id;
+     console.log(clickedId);
+  //const destination div
+     const container = document.querySelector(".destination");
+  
+  //add image
+     
+     const sportImg = document.createElement("div");
+     sportImg.classList.add("img");
+     sportImg.style.backgroundImage = `url("${sportContent[clickedId].picture}")`;
+     
+   
+     container.appendChild(sportImg);
+  
+  //add title
+     const title = document.createElement("h1");
+     title.innerText = sportContent[clickedId].name;
+     container.appendChild(title);
+  
+  //add paragraph
+    const sportIntro = document.createElement("p");
+    sportIntro.innerHTML = sportContent[clickedId].intro;
+    container.appendChild(sportIntro);
+  }
+  
+  
+  
+  
+  
 
 
 
