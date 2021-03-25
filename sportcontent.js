cb08c37def95cc96ea981798a72d5571ea8c2ead
Francesco <france.luciani@gmail.com>
2021-03-25 22:06:00 +0100

    fix contact mobile

--- sportcontent.js
+++ sportcontent.js
@@ -1,23 +1,29 @@
 const sportContent = [
   {
     name: " Tennis",
-    picture: "https://picsum.photos/200/300",
-    intro: "THE ONLY PERSON YOU SCHOULD TRY TO BE BETTER THAN, IS THE PERSON YOU WERE YESTERDAY - NOVAK DJOKOVIC"
-
+    picture:
+      "https://images.pexels.com/photos/1432039/pexels-photo-1432039.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    intro:
+      "THE ONLY PERSON YOU SCHOULD TRY TO BE BETTER THAN, IS THE PERSON YOU WERE YESTERDAY - NOVAK DJOKOVIC",
   },
   {
-    name:" Swimming",
-    picture:"https://picsum.photos/200/300",
-    intro:"DO NOT PUT A LIMIT ON ANYTHING. THE MORE YOU DREAM, THE FURTHER YOU GET - MICHAEL PHELPS",
+    name: " Swimming",
+    picture:
+      "https://images.pexels.com/photos/1263348/pexels-photo-1263348.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    intro:
+      "DO NOT PUT A LIMIT ON ANYTHING. THE MORE YOU DREAM, THE FURTHER YOU GET",
   },
   {
     name: " Climbing",
-    picture: "https://picsum.photos/200/300",
-    intro:"THE BEST PART OF CLIMBING IS WHEN IT CLICKS AND GRAVITY CEASES TO EXIST - CHRIS SHARMA",
+    picture:
+      "https://images.pexels.com/photos/461593/pexels-photo-461593.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    intro:
+      "THE BEST PART OF CLIMBING IS WHEN IT CLICKS AND GRAVITY CEASES TO EXIST.",
   },
   {
-    name:"RUNNING",
-    picture:"https://picsum.photos/200/300",
-    intro:"ONE RUN CAN CHANGE YOUR DAY. MANY RUNS CAN CHANGE YOUR LIFE.",
+    name: "RUNNING",
+    picture:
+      "https://images.pexels.com/photos/4775192/pexels-photo-4775192.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    intro: "ONE RUN CAN CHANGE YOUR DAY. MANY RUNS CAN CHANGE YOUR LIFE.",
   },
 ];
